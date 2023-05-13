2296f93f377154bdfbd46cb284c607eb4cfc6f4d
icexmoon <icexmoon@qq.com>
2023-05-13 15:32:29 +0800

    ioc

--- ch27/ioc/src/test/java/com/example/ioc/IocApplicationTests.java
+++ ch27/ioc/src/test/java/com/example/ioc/IocApplicationTests.java
@@ -3,6 +3,19 @@ package com.example.ioc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : 魔芋红茶
+ * @version : 1.0
+ * @Project : ioc
+ * @Package : com.example.ioc
+ * @ClassName : .java
+ * @createTime : 2023/5/13 15:31
+ * @Email : icexmoon@qq.com
+ * @Website : https://icexmoon.cn
+ * @Description :
+ */
 @SpringBootTest
 class IocApplicationTests {
 
